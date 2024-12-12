831332d4489a6c41f136d4409b8ef960e3804457
maglovskiNenad <maglovskin@gmail.com>
2024-12-12 12:52:35 +0100

    copy btn

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,24 @@ $(document).ready(function () {
     });
   });
 });
+
+$("#copyBtn").on("click", function () {
+  const passwordInput = $("#generatedPassword");
+  const copyMsg = $("#copyMsg");
+
+  if (passwordInput.val().trim() == "") {
+    copyMsg.text("Please generate a password first.").css("color", "red");
+    return;
+  }
+
+  passwordInput.select();
+  passwordInput[0].setSelectionRange(0, 99999);
+
+  try {
+    document.execCommand("copy");
+    copyMsg.text("Password copied to clipboard!").css("color", "black");
+  } catch (error) {
+    copyMsg.text("Failed to copy password.").css("color", "red");
+  }
+  setTimeout(() => copyMsg.text(""), 3000);
+});
